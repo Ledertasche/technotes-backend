8cc9209752d0b5e32baddd1eae860b6b21146ec3
Stefan Bauer <bauersteff58@gmx.de>
2025-09-05 20:55:03 +0200

    Initial backend commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.google.com',
+    'https://google.com',
+    'https://www.weingut-schweikart.de',
+    'https://technotes.onrender.com'
+]
+
+module.exports = allowedOrigins
